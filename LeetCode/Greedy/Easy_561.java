a013d4b504f0d635adff88d0809110a174e47b28
JayFreemandev <jayfreemandev@gmail.com>
2022-10-24 18:56:30 +0900

    Greedy[EASY] 561 ING

---
+++ LeetCode/Greedy/Easy_561.java
@@ -0,0 +1,17 @@
+/**
+ * Created at 2022-10-24
+ * Site -> LINKEDIN
+ * Link -> https://leetcode.com/problems/array-partition/
+ * Level -> EASY
+ * Topic -> GREEDY
+ */
+public class Easy_561 {
+    public static void main(String[] args) {
+        int[] ints = new int[]{1,5,2,4,1};
+        System.out.println(arrayPairSum(ints));
+    }
+
+    public static int arrayPairSum(int[] nums) {
+        return 0;
+    }
+}
