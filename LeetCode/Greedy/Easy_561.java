dde8cda3d8a8d1e869b9c5ab84b397af72d48b8f
jaeyun <jayfreemandev@gmail.com>
2022-10-24 23:38:37 +0900

    Greedy[EASY] 561

--- LeetCode/Greedy/Easy_561.java
+++ LeetCode/Greedy/Easy_561.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created at 2022-10-24
  * Site -> LINKEDIN
@@ -7,11 +9,18 @@
  */
 public class Easy_561 {
     public static void main(String[] args) {
-        int[] ints = new int[]{1,5,2,4,1};
+        int[] ints = new int[]{6,2,6,5,1,2};
         System.out.println(arrayPairSum(ints));
     }
 
     public static int arrayPairSum(int[] nums) {
-        return 0;
+        Arrays.sort(nums);
+        int sum=0;
+
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += nums[i];
+        }
+
+        return sum;
     }
 }
