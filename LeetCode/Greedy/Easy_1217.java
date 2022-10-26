48537964d7eaceb06eaf2a99a11a826effdbd359
JayFreemandev <jayfreemandev@gmail.com>
2022-10-26 12:56:06 +0900

    Greedy[EASY] 1217 ING

---
+++ LeetCode/Greedy/Easy_1217.java
@@ -0,0 +1,18 @@
+/**
+ * Created at 2022-10-26
+ * Site -> LINKEDIN
+ * Link -> Minimum Cost to Move Chips to The Same Position
+ * Level -> EASY
+ * Topic -> GREEDY
+ */
+public class Easy_1217 {
+    public static void main(String[] args) {
+
+        int[] position = {2,2,2,3,3};
+        System.out.println(minCostToMoveChips(position));
+    }
+
+    public static int minCostToMoveChips(int[] position) {
+
+    }
+}
