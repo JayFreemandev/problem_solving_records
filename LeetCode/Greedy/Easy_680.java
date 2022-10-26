fb52ce2777d4a00d91de5b469f51d7e3bd184f53
jaeyun <jayfreemandev@gmail.com>
2022-10-26 22:51:21 +0900

    Greedy[EASY] 680 ING

---
+++ LeetCode/Greedy/Easy_680.java
@@ -0,0 +1,17 @@
+/**
+ * Created at 2022-10-27
+ * Site -> LINKEDIN
+ * Link -> https://leetcode.com/problems/valid-palindrome-ii/
+ * Level -> EASY
+ * Topic -> GREEDY
+ */
+public class Easy_680 {
+    public static void main(String[] args) {
+        String word = "bza";
+        System.out.println(validPalindrome(word));
+    }
+
+    public static boolean validPalindrome(String s) {
+        return false;
+    }
+}
