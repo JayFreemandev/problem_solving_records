55430538b9f4ca602cee1f5adc525541257db8c3
jaeyun <jayfreemandev@gmail.com>
2022-12-04 18:07:42 +0900

    GREEDY[MEDIUM] 921 ing

--- LeetCode/Greedy/MEDIUM_1605.java
+++ LeetCode/Greedy/MEDIUM_1605.java
@@ -2,10 +2,10 @@
  * Created at 2022-12-04
  * Site -> LINKEDIN
  * Link -> https://leetcode.com/problems/find-valid-matrix-given-row-and-column-sums/
- * Level -> EASY
+ * Level -> MEDIUM
  * Topic -> GREEDY
  */
-public class Easy_1605 {
+public class MEDIUM_1605 {
     public static void main(String[] args) {
         int[] rowSum = {3,8}, colSum = {4,7};
         System.out.println(restoreMatrix(rowSum, colSum));
---
+++ LeetCode/Greedy/MEDIUM_921.java
@@ -0,0 +1,17 @@
+/**
+ * Created at 2022-12-04
+ * Site -> LINKEDIN
+ * Link -> https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+ * Level -> MEDIUM
+ * Topic -> GREEDY
+ */
+public class MEDIUM_921 {
+    public static void main(String[] args) {
+        String s = "())";
+        System.out.println(minAddToMakeValid(s));
+    }
+
+    public static int minAddToMakeValid(String s) {
+
+    }
+}
