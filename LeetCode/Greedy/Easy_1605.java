6c5515e85cb07f156500513cbdbaae939a722df9
jaeyun <jayfreemandev@gmail.com>
2022-12-04 17:07:10 +0900

    GREEDY[MEDIUM] 1605 ing

---
+++ LeetCode/Greedy/Easy_1605.java
@@ -0,0 +1,21 @@
+/**
+ * Created at 2022-12-04
+ * Site -> LINKEDIN
+ * Link -> https://leetcode.com/problems/find-valid-matrix-given-row-and-column-sums/
+ * Level -> EASY
+ * Topic -> GREEDY
+ */
+public class Easy_1605 {
+    public static void main(String[] args) {
+        int[] rowSum = {3,8}, colSum = {4,7};
+        System.out.println(restoreMatrix(rowSum, colSum));
+    }
+
+    public static int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+
+        // ...
+
+
+        return null;
+    }
+}
