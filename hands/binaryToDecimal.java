6b4740ece87c0bede2287c902d94810f0841ac7e
jay <jayfreemandev@gmail.com>
2023-06-09 22:02:47 +0900

    binary -> decimal -> octal

---
+++ hands/binaryToDecimal.java
@@ -0,0 +1,6 @@
+public class binaryToDecimal {
+    public static void main(String[] args) {
+        int decimal = Integer.valueOf("1010", 2);
+        System.out.println(decimal);
+    }
+}
---
+++ hands/octalToDecimal.java
@@ -0,0 +1,6 @@
+public class octalToDecimal {
+    public static void main(String[] args) {
+        int decimal = Integer.parseInt("2A", 16);
+        System.out.println(decimal);
+    }
+}
