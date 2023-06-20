ee5a1f8db64c9a72dcd735390057fd1e1623e9ac
JayFreeman <jayfreemandev@gmail.com>
2023-06-20 16:57:37 +0900

    Time: 1 ms (100.00%) | Memory: 51.9 MB (55.96%) - LeetSync

--- product-of-array-except-self/product-of-array-except-self.java
+++ product-of-array-except-self/product-of-array-except-self.java
@@ -1,16 +1,21 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] ans = new int[nums.length];
-        int temp = 1;
-        for(int i = 0; i< nums.length; i++) {
-            ans[i] = temp;
-            temp *= nums[i];
+        int n = nums.length;
+        int[] result = new int[n];
+        
+        // 왼쪽 요소들의 곱 계산
+        result[0] = 1;
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i-1] * nums[i-1];
         }
-        temp = 1;
-        for(int i = nums.length-1; i >= 0; i--) {
-            ans[i] *= temp;
-            temp *= nums[i];
+        
+        // 오른쪽 요소들의 곱 계산 및 결과 계산
+        int rightProduct = 1;
+        for (int i = n-1; i >= 0; i--) {
+            result[i] *= rightProduct;
+            rightProduct *= nums[i];
         }
-        return ans;
+        
+        return result;
     }
 }
